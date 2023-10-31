31beaddd057e6f9d1ec059bb77f74335fb2f2f75
ishanrana363 <ishanrana363@gmail.com>
2023-11-01 00:55:34 +0600

    useState Understing Inside

--- src/Component/UseStateUndesting.jsx
+++ src/Component/UseStateUndesting.jsx
@@ -1,8 +1,11 @@
-const UseStateUndesting = () => {
+import { useState } from "react";
 
+const UseStateUndesting = () => {
+    const [number,setNumber] = useState(0)
     return (
         <div>
-
+            <h1>{number}</h1>
+            <button onClick={()=>setNumber(number+1)} >Increment</button>
         </div>
     );
 };
